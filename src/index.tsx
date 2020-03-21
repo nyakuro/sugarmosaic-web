15de39d0a3078e31138fb7090c458f30977515fc
nyakuro <nyakuro@pixiv.com>
2020-03-21 18:10:11 +0900

    react-router-dom追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import App from "components/App";
+import App from "pages/app";
 import store from "store";
 import * as serviceWorker from "./serviceWorker";
 
